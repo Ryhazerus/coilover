3ca29f994db51fc1811623470e233a6c3a08a62e
Zahyr Seferina <zahyr.seferina@gmail.com>
2019-04-09 00:27:43 +0200

    added metadata implementation from yml file

---
+++ src/generate/mod.rs
@@ -0,0 +1,74 @@
+use std::fs;
+use yaml_rust::{YamlEmitter, YamlLoader};
+
+enum Conversion {
+    h1,
+    h2,
+    h3,
+    p,
+}
+
+pub struct Generate {
+    pub directory_path: String,
+    pub template_path: String,
+    pub build_directory: String,
+}
+
+impl Generate {
+    pub fn new(path: &str, template_path: &str, build_directory: &str) -> Generate {
+        Generate {
+            directory_path: String::from(path),
+            template_path: String::from(template_path),
+            build_directory: String::from(build_directory),
+        }
+    }
+
+    fn read_raw_files(path: &String) -> String {
+        fs::read_to_string(path).expect("Unable to read file")
+    }
+
+    pub fn generate_template(path: String) {
+        if let Ok(entries) = fs::read_dir(path) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    if let Ok(metadata) = entry.metadata() {
+                        let path: String =
+                            String::from(entry.path().into_os_string().into_string().unwrap());
+                        Generate::include_in_template(&Generate::read_raw_files(&path))
+                    } else {
+                        println!("Couldn't get metadata for {:?}", entry.path());
+                    }
+                }
+            }
+        }
+    }
+
+    fn include_in_template(file: &String) {}
+
+    pub fn include_metadata_in_template(config_path: &String, template_path: &String) {
+        // Load the yaml config file
+        let config = YamlLoader::load_from_str(&Generate::read_raw_files(config_path)).unwrap();
+
+        // Load the template file we want to manipulate
+        let template = Generate::read_raw_files(&template_path);
+
+        // Load all the variables from the yml file
+        let title: &str = config[0]["title"][0].as_str().unwrap();
+        let description: &str = config[0]["description"][0].as_str().unwrap();
+        let author: &str = config[0]["author"][0].as_str().unwrap();
+
+        // Parse the loaded yml file into the template file
+        let mut buffer = template.replace("{{%author%}}", author);
+        buffer = buffer.replace("{{%title%}}", title);
+        buffer = buffer.replace("{{%description%}}", description);
+
+        // Save the newly generated file to disk
+        Generate::save_template(&String::from("./dist"), &buffer);
+    }
+
+    fn save_template(path: &String, file_contents: &String) {
+        fs::create_dir_all(path).expect("error cannot create dir");
+        let filename: String = path.as_str().to_owned() + "/index.htm";
+        fs::write(filename, file_contents).expect("Unable to write file");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,40 @@
 extern crate clap;
+extern crate yaml_rust;
+
+mod config;
+mod generate;
 
 use clap::{App, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("Wastegate")
-                        .version("1.0")
-                        .author("Zahyr S. <zahyr.seferina@gmail.com>")
-                        .about("Rust based static site generator, with no extra fat.")
-                        .arg(Arg::with_name("config")
-                                    .short("c")
-                                    .long("config")
-                                    .value_name("FILE")
-                                    .help("Sets a custom config file")
-                                    .takes_value(true))
-                        .get_matches();
+        .version("1.0")
+        .author("Zahyr S. <zahyr.seferina@gmail.com>")
+        .about("Rust based static site generator, with no extra fat.")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("generate")
+                .short("g")
+                .long("generate")
+                .help("will generate a new static site"),
+        )
+        .get_matches();
 
     if let Some(c) = matches.value_of("config") {
         println!("Value for config: {}", c);
     }
 
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level app
-    if let Some(matches) = matches.subcommand_matches("test") {
-        // "$ myapp test" was run
-        if matches.is_present("list") {
-            // "$ myapp test -l" was run
-            println!("Printing testing lists...");
-        } else {
-            println!("Not printing testing lists...");
-        }
+    if matches.is_present("generate") {
+        let test = generate::Generate::include_metadata_in_template(
+            &String::from("./src/mock/config/wastegate.yml"),
+            &String::from("./src/templates/index.htm"),
+        );
     }
-
-
-    // Continued program logic goes here...
 }
